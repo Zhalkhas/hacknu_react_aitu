59c00847dcd6c258e371d567645792be7c19426c
Ablan Abkenov <abkenov.ablan@gmail.com>
2021-03-21 04:12:37 +0600

    Deleted unused component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,16 +4,12 @@ import {
   IonLabel,
   IonList,
   IonItem,
-  IonListHeader,
   IonToolbar,
   IonSearchbar,
   IonContent,
   IonHeader,
   IonTitle,
   IonPage,
-  IonSegment,
-  IonSegmentButton,
-  IonRouterOutlet,
   IonRouterLink,
   IonAvatar,
   IonBadge,
@@ -32,7 +28,6 @@ import {
   IonCardContent,
   IonCardTitle
 } from "@ionic/react";
-import { Route, Redirect } from 'react-router';
 import { RefresherEventDetail } from '@ionic/core';
 
 /* Core CSS required for Ionic components to work properly */
@@ -50,14 +45,6 @@ import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
-import { IonReactRouter } from "@ionic/react-router";
-import { Switch } from "react-router";
-import { chevronForward, chevronForwardOutline, helpCircleOutline, peopleOutline, pricetagsOutline, refresh } from "ionicons/icons";
-import { NONAME } from "node:dns";
-import FriendsList from "./FriendsList";
-
-/* Theme variables */
-// import "../theme/variables.css";
 
 const Profile: React.FC = () => {
     const [name, setName] = useState('<name>')
